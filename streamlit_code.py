ed105e89029463a5ef171d7439d1b5158fef5988
Mahnoor Sheikh <67545978+mahnoorsheikh16@users.noreply.github.com>
2025-04-21 22:55:45 -0400

    Update streamlit_code.py

--- streamlit_code.py
+++ streamlit_code.py
@@ -53,7 +53,7 @@ else:
 #add navigation sidebar
 st.sidebar.title("🔎Explore")
 page = st.sidebar.selectbox("Select a page:", ["Homepage", "Evaluate Text", "Model & Insights"], index=0)
-for _ in range(16):
+for _ in range(20):
     st.sidebar.write("")
 st.sidebar.write("View the code and dataset details at [STT811 GitHub](https://github.com/andrew-jxhn/STT811_StatsProject)")
 
