e9db6b579ee92f6edfd081787dc20d7e51672e40
Andrew John J <68630018+andrew-jxhn@users.noreply.github.com>
2025-04-24 08:48:18 -0400

    Update streamlit_code.py

--- streamlit_code.py
+++ streamlit_code.py
@@ -363,13 +363,14 @@ elif page == "Model & Insights":
         imageida11 = Image.open("Streamlit/visualizations/FLesh_score.png")
         st.image(imageida11, use_column_width=True)
         st.subheader("Flesch Reading Ease (FRE)")
-        st.markdown(r"""
-        <div style='color:white; font-size: 20px;'>
-        $$
-        \mathrm{FRE}(X) = 206.835 - 1.015\,\frac{W}{S} - 84.6\,\frac{\mathrm{SYL}}{W}
-        $$
-        </div>
-        """, unsafe_allow_html=True)
+        st.latex(r"""
+        \mathrm{FRE}(X)
+        = 206.835
+        \;-\
+        1.015\,\frac{W}{S}
+        \;-\
+        84.6\,\frac{\mathrm{SYL}}{W}
+        """)
         
         st.subheader("Gunning Fog Index (GFI)")
         st.latex(r"""
